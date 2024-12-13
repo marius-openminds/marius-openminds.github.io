dba4677b3684a4037c900b02d6554a7c943aaf40
marius thøgersen <mt@openminds.dk>
2024-12-13 08:15:10 +0100

    epileptic dog

--- scripts.js
+++ scripts.js
@@ -3,11 +3,26 @@
 addEventListener("DOMContentLoaded", () => {
     const card = document.querySelector(".card")
     console.log(card)
-    
-    let current = 0;
-    const colors = ["gold","dodgerblue", "rebeccapurple", "red"]
-    setInterval(()=> card.style.setProperty("--start", colors[current++ % colors.length]), 1000);
 
+
+
+    const colors = ["gold","#fff", "dodgerblue","#0f12d2", "rebeccapurple", "red","tomato", "purple"];
+    const interval = 500;
+
+    let currentStart = 0;
+    let currentMiddle = colors.length-2;
+    let currentEnd = colors.length / 2;
+    let tilt = 45;
+
+
+    setInterval(
+        () => { 
+            card.style.setProperty("--start", colors[currentStart++ % colors.length]) 
+            card.style.setProperty("--middle", colors[currentMiddle++ % colors.length]) 
+            card.style.setProperty("--end", colors[currentEnd++ % colors.length]) 
+            card.style.setProperty("--tilt", `${tilt+=37 % 360}deg`) 
+        },
+        interval);
 })
 
 
