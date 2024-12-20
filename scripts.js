2aae52f8423e9e2a7985eaea862d0257999144a3
marius-openminds <mt@openminds.dk>
2024-12-20 13:48:16 +0100

    smoothness

--- scripts.js
+++ scripts.js
@@ -16,22 +16,26 @@ addEventListener("DOMContentLoaded", () => {
 
 
     let angle = 45;
+    let hueOffset = 0;
 
     rotate();
 
     function rotate(){
         angle = (angle + 1) % 360;
+        hueOffset = (hueOffset + 1) % 360;
         card.style.setProperty("--rotation", `${angle}deg`)
+        card.style.setProperty("--hueoffset", `${hueOffset}`)
         requestAnimationFrame(rotate)
     }
 
-    setInterval(
-        () => { 
-            card.style.setProperty("--start", colors[currentStart++ % colors.length]) 
-            card.style.setProperty("--middle", colors[currentMiddle++ % colors.length]) 
-            card.style.setProperty("--end", colors[currentEnd++ % colors.length]) 
-        },
-        colorInterval);
+    // setInterval(
+    //     () => { 
+    //         card.style.setProperty("--start", colors[currentStart++ % colors.length]);
+    //         card.style.setProperty("--middle", colors[currentMiddle++ % colors.length]) ;
+    //         card.style.setProperty("--end", colors[currentEnd++ % colors.length]);
+
+    //     },
+    //     colorInterval);
 })
 
 
